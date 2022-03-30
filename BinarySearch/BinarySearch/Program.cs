cab7965d5021b26fe0c5cf5b84eb829870412555
Remant <remantmahato9798@gmail.com>
2022-03-30 23:40:46 +0530

    Added : UC1-CreateBinarySearchTree

---
+++ BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Binary search tree problem ");
+            ViewBST();
+            Console.ReadLine();
+        }
+
+        public static void ViewBST()
+        {
+            MyBinaryTree<int> tree = new MyBinaryTree<int>();
+            tree.CreateNode(56);
+            tree.CreateNode(30);
+            tree.CreateNode(70);
+            Console.WriteLine("Display Nodes present in Binary Search Tree");
+            tree.Display();
+        }
+    }
+
+}
